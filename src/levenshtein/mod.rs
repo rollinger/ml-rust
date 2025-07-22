1ea746d6fce3231f61b5740ebc2a413e1ca61a2e
Philipp Rollinger <philipp.rollinger@gmail.com>
2025-07-22 12:39:35 +0700

    todos

--- src/levenshtein/mod.rs
+++ src/levenshtein/mod.rs
@@ -3,6 +3,10 @@
  * - distance(s1: &str, s2: &str)  => distance_unicode(...)
  * - distance_unicode(s1: &str, s2: &str) -> usize
  * - distance_byte(s1: &str, s2: &str) -> usize
+ * 
+ * TODO:
+ * - Implement Interpretations::Words (tokenize on whitespace)
+ * - Implement Interpretations::NGram(w) (tokenize on width)
  */
 
 mod distance;
